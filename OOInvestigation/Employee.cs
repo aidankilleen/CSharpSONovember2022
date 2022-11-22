8f051063c77ee9b7be7451bb795be4acfc159e29
Aidan Killeen <aidan.killeen@gmail.com>
2022-11-22 10:55:55 +0000

    added hr system and first two classes

---
+++ OOInvestigation/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOInvestigation
+{
+    internal class Employee : Person
+    {
+        public string EmployeeId { get; set; }
+
+        public Employee(string firstName, 
+                string lastName, 
+                string employeeId)
+            :base(firstName, lastName)
+        {
+            //this.FirstName = firstName;
+            //this.LastName = lastName;
+            this.EmployeeId = employeeId;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("Employee:");
+            //Console.WriteLine(FirstName + " " +  LastName);
+            base.Display();
+            Console.WriteLine("EmployeeId:"+ EmployeeId);
+        }
+    }
+}
---
+++ OOInvestigation/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OOInvestigation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OO Investigation");
+
+
+            Person p = new Person("Aidan", "Killeen");
+            p.Display();
+
+            Employee e = new Employee("Alice", 
+                                    "Adams", 
+                                      "E12345");
+            e.Display();
+            
+
+            // Abstraction (the ability to create an object)
+
+
+            /* Encapsulation (public & private keywords - 
+             * the ability to protect the elements of the object
+             * from the outside world)
+             */
+
+            // Inheritance
+
+            // Polymorphism
+
+        }
+    }
+}
