734ba09ecb72142959808da8b646cce7f7a4d0b1
Aidan Killeen <aidan.killeen@gmail.com>
2022-11-25 12:29:07 +0000

    Added LinqInvestigation with records

---
+++ LinqInvestigation/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Security.Cryptography;
+using UserDaoLibrary;
+
+namespace LinqInvestigation
+{
+
+    class Record
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public int Quantity { get; set; }
+
+        public Record(int id, string name, string colour, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Colour = colour;
+            Quantity = quantity;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linq Investigation");
+
+            int[] arr = { 2, 5, 7, 6, 4, 8, 10, 9, 2, 3, 4, 15, 11, 7 };
+            List<int> list = new List<int>(arr);
+
+            list.ForEach(i => Console.WriteLine(i));
+
+            // the var keyword
+            var i = 10;
+            var even = list.FindAll(i => i % 2 == 0);
+
+
+            // var j;   // you can't create a var variable without initialising it
+            // i = "ten";  // the type safety means I can't change the type.
+
+            Console.WriteLine("=============================");
+            // very first linq expression
+            IEnumerable<int> result = from item in list select item;
+            foreach(int item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("==============================");
+            result = from item in list where item % 2 == 0 select item;
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("==============================");
+            var evenNumbers = list.Where(i => i % 2 == 0);
+            
+            foreach (int item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("==============================");
+
+
+            SqliteUserDao dao = new SqliteUserDao();
+
+            var users = dao.GetUsers();
+            var activeUsers = from user in users
+                                where user.Active == true
+                                orderby user.Name descending
+                                select user;
+
+            foreach (var user in activeUsers)
+            {
+                Console.WriteLine(user);
+            }
+
+
+            Console.WriteLine("=====================================");
+            var agg = from user in users
+                      group user by user.Active into g
+                      select new
+                      {
+                          Active = g.Key,
+                          Count = g.Count()
+                          
+                      };
+            foreach (var item in agg)
+            {
+                Console.WriteLine($"{(item.Active ? "Active:   " : "Inactive:")}\t{ item.Count }");
+            }
+
+            dao.Close();
+
+
+            List<Record> data = new List<Record>()
+            {
+                new Record (1, "Alice", "red", 100),
+                new Record(2, "Bob", "green", 12),
+                new Record(3, "Carol", "blue", 14),
+                new Record(4, "Dan", "red", 10),
+                new Record(5, "Carol", "green", 16),
+                new Record(6, "Dan", "blue", 10),
+                new Record(7, "Bob", "green", 1),
+                new Record(8, "Alice", "green", 20),
+                new Record(9, "Bob", "green", 15),
+                new Record(10, "Carol", "red", 10),
+                new Record(11, "Alice", "red", 10),
+                new Record(12, "Bob", "green", 12),
+                new Record(13, "Carol", "blue", 14),
+                new Record(14, "Dan", "red", 10),
+                new Record(15, "Carol", "green", 16),
+                new Record(16, "Dan", "blue", 10),
+                new Record(17, "Bob", "orange", 1),
+                new Record(18, "Alice", "green", 20),
+                new Record(19, "Bob", "green", 15),
+                new Record(20, "Carol", "red", 10),
+                new Record(21, "Eve", "orange", 99)
+            };
+
+        }
+    }
+}
---
+++ TupleInvestigation/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace TupleInvestigation
+{
+    class Results
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+
+    class Card
+    {
+        public int Suit { get; set; }
+        public int Value { get; set; }
+
+        public Card(int suit, int value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+
+    internal class Program
+    {
+        public static Results DoCalculations(int[] list)
+        {
+            int max = list.Max(x => x);
+            int min = list.Min(x => x);
+
+            Results results = new Results();
+            results.Max = max;
+            results.Min = min;
+            return results;
+        }
+
+        public static (int, int) DoListCalculations(int[] list)
+        {
+            int max = list.Max(x => x);
+            int min = list.Min(x => x);
+
+            return (min, max);
+        }
+
+        public static (int Min, int Max) DoListAnalysis(int[] list)
+        {
+            int max = list.Max(x => x);
+            int min = list.Min(x => x);
+
+            return (min, max);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuple Investigation");
+            int[] list = { 2, 6, 3, 9, 10, 8, 3, 1 };
+            Results answer = DoCalculations(list);
+            Console.WriteLine($"The answer is { answer.Min }, { answer.Max }");
+            
+            (int, int) results = DoListCalculations(list);
+            Console.WriteLine($"The answers are { results.Item1 }, { results.Item2 }");
+            (int Min, int Max) analysis = DoListAnalysis(list);
+            
+            Console.WriteLine($"The analysis is { analysis.Min }, { analysis.Max }");
+
+
+            int a = 100;
+            int b = 10;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            (a, b) = (b, a); // swap two variable using a tuple - no need for a third temporary variable
+
+            Console.WriteLine($"a = { a }, b = { b }");
+
+
+
+            Card c1 = new Card(1, 1);
+
+            Card c2 = new Card(13, 3);
+
+            Console.WriteLine($"{c1.Suit}, {c1.Value}");
+            Console.WriteLine($"{c2.Suit}, {c2.Value}");
+
+            (c1, c2) = (c2, c1);
+
+            Console.WriteLine($"{c1.Suit}, {c1.Value}");
+            Console.WriteLine($"{c2.Suit}, {c2.Value}");
+            
+            (int, double, string) t = (1, 1.1, "one");
+
+            t.Item1 = 10;
+
+            Console.WriteLine(t);
+
+
+
+
+        }
+    }
+}
